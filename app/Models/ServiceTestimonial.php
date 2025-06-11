a1caa064c8b2754e01323f2a0b2d26ba9c300588
Duta <dutaalif.20091@mhs.unesa.ac.id>
2025-06-11 13:16:56 +0700

    first commit

---
+++ app/Models/ServiceTestimonial.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ServiceTestimonial extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'photo',
+        'message',
+        'home_service_id',
+    ];
+
+    public function homeService()
+    {
+        return $this->belongsTo(HomeService::class, 'home_service_id');
+    }
+}
